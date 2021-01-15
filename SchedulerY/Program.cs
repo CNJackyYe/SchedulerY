ccf1ca21cd51c2e8c92d436b3cb44b8cd442b5c1
Jacky Ye <yeqw321@163.com>
2021-01-15 17:00:26 +0800

    github 11

--- SchedulerY/Program.cs
+++ SchedulerY/Program.cs
@@ -18,9 +18,6 @@ namespace SchedulerY
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
